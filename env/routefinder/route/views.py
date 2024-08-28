fc957468698684dcef2fd91582414110527403c0
Dominic Brzezinski <dombrzezinski@icloud.com>
2024-08-28 21:15:31 +0100

    Added test scenario

--- env/routefinder/route/views.py
+++ env/routefinder/route/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from . import router
+from . import coordinates as router
 import os
 
 def getRoute(request):
