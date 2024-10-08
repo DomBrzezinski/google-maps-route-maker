a8969f836788823075f3f7d7679528d69858fa66
Dominic Brzezinski <dombrzezinski@icloud.com>
2024-10-08 21:21:36 +0100

    Added form functionality

--- env/routeFinder/route/forms.py
+++ env/routeFinder/route/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
 class dataForm(forms.Form):
-    data = forms.CharField()
+    # data = forms.CharField()
+    starting_location = forms.CharField(label="Starting Location: ")
+    return_original = forms.BooleanField(label="Return back here?")
+    ending_location = forms.CharField(label="Ending Location: ")
+    travel_method = forms.ChoiceField(widget=forms.RadioSelect, choices=[('1','Walking'),('2','Running'),('3','Bicycling')])
+    time_or_distance = forms.ChoiceField(widget=forms.RadioSelect, choices=[('1','Time'),('2','Distance')])
+    time_input = forms.IntegerField(label="Enter the time in minutes: ")
+    distance_input = forms.FloatField(label="Enter the distance in kilometres: ")
+    
