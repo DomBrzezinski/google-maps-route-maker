8810cede34209f6a00129b1fa9b3578d07052f0a
Dominic Brzezinski <dombrzezinski@icloud.com>
2024-11-27 22:32:53 +0000

    updated marker icons

--- env/routefinder/route/urls.py
+++ env/routefinder/route/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('route/', views.getRoute, name='route' ),
     path('inputs/', views.inputs, name='inputs')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
